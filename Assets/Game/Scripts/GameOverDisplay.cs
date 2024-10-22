a8c96a1fe9e61734f77583c925b391f8e1cb3476
LAPTOP-2IR7PPSS\almaz <almazfardiev080@gmail.com>
2024-10-22 21:05:17 +0300

    Added exit to menu button

--- Assets/Game/Scripts/GameOverDisplay.cs
+++ Assets/Game/Scripts/GameOverDisplay.cs
@@ -1,23 +1,37 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverDisplay : MonoBehaviour
 {
     [SerializeField] Canvas _canvas;
     [SerializeField] TMP_Text _text;
+    [SerializeField] Button _button;
+
+    public static event Action ExitMenuClicked;
 
     private void OnEnable()
     {
         _canvas.enabled = false;
         GameOverHandler.RpcGameOver += DisplayWinner;
+        _button.onClick.AddListener(OnExitMenuClicked);
     }
 
     private void OnDisable()
     {
         GameOverHandler.RpcGameOver -= DisplayWinner;
+        _button.onClick.RemoveListener(OnExitMenuClicked);
+    }
+
+    private void OnExitMenuClicked()
+    {
+        _canvas.enabled = false;
+        ExitMenuClicked?.Invoke();
     }
 
     private void DisplayWinner(string name)
--- Assets/Game/Scripts/SnakeNetworkManager.cs
+++ Assets/Game/Scripts/SnakeNetworkManager.cs
@@ -8,6 +8,32 @@ public class SnakeNetworkManager : NetworkManager
     [SerializeField] FoodSpawner _foodSpawnerPrefab;
     [SerializeField] GameOverHandler _gameOverHandlerPrefab;
 
+    private void OnEnable()
+    {
+        GameOverDisplay.ExitMenuClicked += ExitMenu;
+    }
+
+    private void OnDisable()
+    {
+        GameOverDisplay.ExitMenuClicked -= ExitMenu;
+    }
+
+    private void ExitMenu()
+    {
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            StopHost();
+        }
+        else if (NetworkServer.active)
+        {
+            StopServer();
+        }
+        else
+        {
+            StopClient();
+        }
+    }
+
     public override void OnStartServer()
     {
         GameOverHandler gameOverHandler = Instantiate(_gameOverHandlerPrefab);
