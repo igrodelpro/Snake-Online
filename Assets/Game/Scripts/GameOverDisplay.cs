e023a6acce452f635346d285c79d014bbb4e7174
LAPTOP-2IR7PPSS\almaz <almazfardiev080@gmail.com>
2024-12-19 18:50:14 +0300

    end

--- Assets/Game/Scripts/GameOverDisplay.cs
+++ Assets/Game/Scripts/GameOverDisplay.cs
@@ -34,12 +34,19 @@ public class GameOverDisplay : MonoBehaviour
             _button.onClick.RemoveListener(OnExitMenuClicked);
         }
     }
-
+    
     private void DisplayWinner(string name)
     {
         SetActive(true);
-        _text.text = name;
+        _text.text = $"winner: {name}";
     }
     //1) Исправить баг: Когда хост нажимает кнопку выхода в меню,
     //сессия останавливается у всех клиентов, но у клиентов не скрывается UI победителя.
+
+    //24.10
+    //Земля в игре сейчас сделана при помощи плоскости (Plane). Отключи эту плоскость,
+    //создай 3Д объект местности (Terrain). Нарисуй на ней текстуру QS-GRASS-8.1 (папка Assets -> Textures),
+    //и нарисуй на ней траву, при помощи изображения GrassFrond01AlbedoAlpha или GrassFrond02AlbedoAlpha (также в папке Assets -> Textures).
+
+    //редизайн GameOverDisplay
 }
